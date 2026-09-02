a9a7b2829c66df38dffc0c69a3132142f52f6ee5
Gustavo Basso <gustavoarruda803@gmail.com>
2026-09-02 15:59:44 -0300

    Usuarios, chamado criados

--- ServiceDeskBot.Domain/Entidades/Chamado.cs
+++ ServiceDeskBot.Domain/Entidades/Chamado.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ServiceDeskBot.Domain.Enums;
 
 namespace ServiceDeskBot.Domain.Entidades
 {
@@ -10,5 +11,26 @@ namespace ServiceDeskBot.Domain.Entidades
         public int Id { get; set; }
         public string NumeroChamado { get; set; }
         
+        public int UsuarioId { get; set; }
+        public int LocalId { get; set; }
+        public int CategoriaId { get; set; }
+        public int? SubCategoriaId { get; set; }
+        public int? EquipamentoId { get; set; }
+        public int? TecnicoId { get; set; }
+        public string Descricao { get; set; }
+        public string Impacto { get; set; }
+
+        public DateTime DataAbertura { get; set; }
+
+        public DateTime? DataInicioAtendimento { get; set; }
+
+        public DateTime? DataConclusao { get; set; }
+
+        public string Diagnostico { get; set; }
+        public string Solucao { get; set; }
+        public string Observacao { get; set; }
+        public StatusChamado Status { get; set; }
+
+        public PrioridadeChamado Prioridade { get; set; }
     }
 }
